7bf50c32e7558617cd08286c955732a22cd87640
Fei Tang <fei.tang.web@gmail.com>
2015-05-11 14:59:41 -0500

    send message to server in cs building

--- background.js
+++ background.js
@@ -1,46 +1,28 @@
 chrome.runtime.onConnect.addListener(function(port) {
   console.assert(port.name == "FROM_CONTENT_SCRIPT");
   port.onMessage.addListener(function(msg) {
-    var data = getRequestBody(msg);
-    postDataToServer(data);
+  // create new xhr object each time to send message
+  // by appending query string in ajax POST
+    postDataToServer(getRequestBody(msg));
   });
 });
 
-var url = "http://localhost/ajax.php";
+var url = "http://128.255.45.160/ajax.php";
 
-var getRequestBody = function(msg) {
+function getRequestBody(msg) {
   var pieces = [];
-
   for (var key in msg) {
     key = encodeURIComponent(key);
     var value = encodeURIComponent(msg[key]);
     pieces.push(key + "=" + value);
   }
-
   return pieces.join("&");
 };
-
-// function callback(data) {
-  // alert(data);
-// }
 
 function postDataToServer(data) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", url);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//   
-  // xhr.onreadystatechange = function() {
-    // if (this.readyState === 4) {
-      // var status = this.status;
-//   
-      // if ((status >= 200 && status < 300) || status === 304) {
-        // callback(this.responseText);
-      // } else {
-        // alert("An error occurred");
-      // }
-    // }
-  // };
-  
   xhr.send(data);
   xhr = null;
 }
